657e2f7a2eee7ec69271e84445393c8824316431
ctschnurr <113189402+ctschnurr@users.noreply.github.com>
2022-10-25 09:46:28 -0300

    Set up structure
    the bones are there, needs some polish and development

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -8,8 +8,88 @@ namespace Loops
 {
     internal class Program
     {
+
+        static bool gameOver;
+        static ConsoleKeyInfo choice;
+
+        static int x;
+        static int y;
         static void Main(string[] args)
         {
+            gameOver = false;
+            x = 5;
+            y = 5;
+
+            while (gameOver == false)
+            {
+                Console.Clear();
+                PlayerDraw(x, y);
+                PlayerUpdate();
+            }
+
+
+
+        }
+
+        static void PlayerDraw(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine("0");
+        }
+
+        static void PlayerUpdate()
+        {
+            choice = Console.ReadKey(true);
+            switch(choice.Key)
+            {
+                case ConsoleKey.Escape:
+                    gameOver = true;
+                    break;
+
+                case ConsoleKey.W:
+                    if (y == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        y--;
+                        break;
+                    }
+
+                case ConsoleKey.S:
+                    if (y >= 30)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        y++;
+                        break;
+                    }
+
+                case ConsoleKey.A:
+                    if (x == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        x--;
+                        break;
+                    }
+
+                case ConsoleKey.D:
+                    if (x >= 15)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        x++;
+                        break;
+                    }
+            }
         }
     }
 }
